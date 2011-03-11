35e191f7579cf68344a00291ea633f3c8fcf2aed
Tarif Kayali <tkkayali@gmail.com>
2011-03-10 21:52:05 -0500

    PUnit.py

---
+++ PEnemy.py
@@ -0,0 +1,43 @@
+from PUnit import Unit
+
+class Enemy(Unit):
+	"""
+	This class will serve to build an enemy.
+	This class inherits from Unit.
+	"""
+	
+	__damage = 0
+	__accuracy = 0
+
+	def __init__ (self, _pos_x, _pos_y, _pos_z, _unit_range, _model):
+		Unit.__init__(self, 10, 2, _pos_x, _pos_y, _pos_z, True, _model,
+		False, _unit_range)
+
+		if range == 1:
+			__damage = 8
+			__accuracy = .90
+
+		elif range == 2:
+			__damage = 5
+			__accuracy = .85
+
+		elif range < 5:
+			__damage = 3
+			__accuracy = .80
+
+		else:
+			__damage = 0
+			__accuracy = 0
+
+	def move(self):
+		#HOLDER METHODS
+		self._pos_x += 1
+		self._pos_y += 1
+		self._pos_z += 1
+		self._AP -= 1
+
+	def to_string(self):
+		Unit.to_string(self)
+
+
+
---
+++ PUnit.py
@@ -0,0 +1,33 @@
+class Unit:
+	"""
+	This class will act as a generalied Unit template.
+	This class also supports 3 sub-classes:
+		class: Sonatu
+		class: Enemy
+		class: Queen
+	"""
+
+	_HP = 0
+	_AP = 0
+	_pos_x = 0
+	_pos_y = 0
+	_pos_z = 0
+	_alive = False
+	_model = None
+	_player = False
+	_unit_range = 0
+
+	def __init__ (self, _HP, _AP, _pos_x, _pos_y, _pos_z, _alive,
+	_model, _player, _unit_range):
+		self._HP = _HP
+		self._AP = _AP
+		self._pos_x = _pos_x
+		self._pos_y = _pos_y
+		self._pos_z = _pos_z
+		self._alive = _alive
+		self._unit_range = _unit_range
+		self._model = _model
+		self._player = _player
+
+	def to_string(self):
+		print "HP: " + str(self._HP) + " AP: " + str(self._AP) + " Model: " + str(self._model) + "\nX Position: " + str(self._pos_x) + " Y Position: " + str(self._pos_y) + " Z Position: " + str(self._pos_z) + "\nIs the unit the player's? " + str(self._player) + "\nIs the unit alive? " + str(self._alive)
