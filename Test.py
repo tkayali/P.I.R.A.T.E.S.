d427d5c129827dc71c2bfaace4d6fc609042f710
Tarif Kayali <tkkayali@gmail.com>
2011-03-16 13:45:25 -0400

    Added Environment testing

--- Test.py
+++ Test.py
@@ -3,23 +3,24 @@ from PSonatu import Sonatu
 from PEnemy import Enemy
 from PQueen import Queen
 from PCombatUI import CombatUI
+from PEnvironment import Environment
 
 #print Unit.__doc__
 #print Sonatu.__doc__
 #print Enemy.__doc__
 
-unit = Unit()
+#unit = Unit()
 #unit.to_string()
-print "\n"
-unit2 = Unit(8,50,123,125,-1,True,None,True,98)
+#print "\n"
+#unit2 = Unit(8,50,123,125,-1,True,None,True,98)
 #unit2.to_string()
 
 #player = Sonatu(None, 0)
 #melee = Enemy(0, 0, 0, 1, None)
 #queen = Queen(None)
 
-player = Sonatu(0, 0, 0, None, 0)
-print player.__doc__
+#player = Sonatu(0, 0, 0, None, 0)
+#print player.__doc__
 
 #combatui = CombatUI()
 #print combatui.get_combat_info_box()
@@ -36,3 +37,9 @@ print player.__doc__
 #queen.to_string()
 #queen.move()
 #queen.to_string()
+
+environment = Environment([1,0,0], [0,1,0], [0,0,1])
+environment2 = Environment()
+
+print environment.__doc__
+print environment.to_string()
