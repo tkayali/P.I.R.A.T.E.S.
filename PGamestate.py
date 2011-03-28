ca5aa82307f5525bc0c9ef6696c1a11b8a8b4f27
Tarif Kayali <tkkayali@gmail.com>
2011-03-28 01:28:40 -0400

    Disabled Sonatu's movement to work on more precise hex movement

--- PGamestate.py
+++ PGamestate.py
@@ -28,33 +28,30 @@ loadPrcFile("Config/Config.prc")
 class PIRATES(ShowBase):
 	def __init__(self):
 		ShowBase.__init__(self)
-		
+		base.disableMouse()		
+
 		self.limbo()
-		base.disableMouse()
 		self.combat_collision_detection()
 		self.mouseTask = taskMgr.add(self.combat_mouse_task, 'combat_mouse_task')
+		
 		self.accept("escape", sys.exit)
 		self.accept("h", self.limbo_hide_all )
 		self.accept("c", self.combat )
 		self.accept("l", self.combat_hide_all )
 
-		#self.accept("x", self.printx)
-		#self.accept("y", self.printy)
-		#self.accept("z", self.printz)
-		#self.accept("h", self.printh)
-		#self.accept("p", self.printp)
-		#self.accept("r", self.printr)
-
+		
 	def limbo(self):
+		#Set up the Limbo Sonatu
 		self.limbo_sonatu = self.loader.loadModel("Models\Sonatu\Sonatu.egg")
 		self.limbo_sonatu.setPos(0, 0, 0)
 		self.limbo_sonatu.setHpr(-90, 0, 0)
 		self.limbo_sonatu.reparentTo(self.render)
-
+		
+		#Set up the camera for the Limbo Menu
 		self.taskMgr.add(self.limbo_camera_task, "Limbo Camera")
 
 	def combat(self):
-		#Let's get some water in here!
+		#Combat water
 		self.water = self.loader.loadModel("square.egg")
 		self.water.setSx(1000)
 		self.water.setSy(1000)
@@ -64,38 +61,36 @@ class PIRATES(ShowBase):
 		self.water.setTexScale(ts,4)
 		self.water.reparentTo(self.render)
 
-		#Sonatu!
+		#Combat Sonatu
 		self.combat_sonatu = self.loader.loadModel("Models\Sonatu\Sonatu.egg")
-		self.combat_sonatu.setSx(.02)
-		self.combat_sonatu.setSy(.02)
-		self.combat_sonatu.setSz(.02)
-		self.combat_sonatu.setPos(16*20*sin(pi/3), 13*15, 0)
-		self.combat_sonatu.lookAt(20*sin(pi/3)*7.5, -15*6.5, 0)
+		self.combat_sonatu.setSx(.025)
+		self.combat_sonatu.setSy(.025)
+		self.combat_sonatu.setSz(.025)
+		self.combat_sonatu.setPos(0, 0, 1)
+		self.combat_sonatu.lookAt(-1000, 0, 0)
 		self.combat_sonatu.reparentTo(self.render)
-		sonatu = Sonatu(0, 0, 0, self.combat_sonatu, 1)
-
-		#Gridspace!
-		gridspace_list = []
+		sonatu = Sonatu(0, 0, 0, self.combat_sonatu, 2)
 
 		#Create the hex grid
+		self.gridspace_list = []
 		y_counter = 0
 		hex_radius = 10
 		for j in range(6):
 			x_counter1 = 0
 			x_counter2 = 10*sin(pi/3)
 			for i in range(16):
-				gridspace_list.append(Gridspace(None, True, x_counter1, y_counter, hex_radius, j*i+i))
+				self.gridspace_list.append(Gridspace(None, True, x_counter1, y_counter, hex_radius, j*i+i))
 				x_counter1 = x_counter1 + 2*hex_radius*sin(pi/3)
 			
 			y_counter = y_counter - 3/2.0*hex_radius
 			for i in range(15):
-				gridspace_list.append(Gridspace(None, True, x_counter2, y_counter, hex_radius, j*i+i+16))
+				self.gridspace_list.append(Gridspace(None, True, x_counter2, y_counter, hex_radius, j*i+i+16))
 				x_counter2 = x_counter2 + 2*hex_radius*sin(pi/3)
 			y_counter = y_counter - 3/2.0*hex_radius
 			if j == 5: 
 				x_counter1 = 0
 				for i in range (16):
-					gridspace_list.append(Gridspace(None, True, x_counter1, y_counter, hex_radius, (j+1)*i+i))
+					self.gridspace_list.append(Gridspace(None, True, x_counter1, y_counter, hex_radius, (j+1)*i+i))
 					x_counter1 = x_counter1 + 2*hex_radius*sin(pi/3)		
 
 		#Add a hex grid texture
@@ -107,7 +102,7 @@ class PIRATES(ShowBase):
 		self.map_grid.setTransparency(TransparencyAttrib.MAlpha)
 		self.map_grid.reparentTo(self.render)				
 
-		#Camera for combat
+		#Set up camera for combat
 		self.taskMgr.add(self.combat_camera_task, "Combat Camera")
 	
 	def limbo_hide_all(self):
@@ -120,21 +115,28 @@ class PIRATES(ShowBase):
 
 	def combat_mouse_task(self, task):
 		if base.mouseWatcherNode.hasMouse():
-			self.accept("mouse1", self.printMouse )
+			self.accept("mouse1", self.move_sonatu )
 		return Task.cont
 
 	def PointAtZ(self, z, point, vec):
+		print point + vec * ((z-point.getZ()) / vec.getZ())
   		return point + vec * ((z-point.getZ()) / vec.getZ())
 	
-	def printMouse(self):
+	def move_sonatu(self):
 		if base.mouseWatcherNode.hasMouse():
 			self.mouse_position = base.mouseWatcherNode.getMouse()
 			self.collision_ray.setFromLens(base.camNode, self.mouse_position.getX(), self.mouse_position.getY())
 			self.near_point = render.getRelativePoint(self.camera, self.collision_ray.getOrigin())
 			self.near_vector = render.getRelativeVector(self.camera, self.collision_ray.getDirection())
-			self.combat_sonatu.setPos(self.PointAtZ(.5, self.near_point, self.near_vector))
-			print self.mouse_position.getX()
-			print self.mouse_position.getY()
+			self.traverser.traverse(render)
+			if self.handler.getNumEntries() > 0:
+				self.handler.sortEntries()
+				hex = self.handler.getEntry(0).getIntoNodePath()
+				hex = hex.findNetTag("hex");
+				self.combat_sonatu.setPos( hex.get_x_position, hex.get_y_position, 1)
+
+			#relative_point = self.PointAtZ(.5, self.near_point, self.near_vector)
+			#self.combat_sonatu.setPos(relative_point)
 
 	def combat_camera_task(self, task):
 		self.camera.setPos(20*sin(pi/3)*7.5, 225, 225)
@@ -156,8 +158,7 @@ class PIRATES(ShowBase):
 		self.collision_ray = CollisionRay()
 		self.collision_node.addSolid(self.collision_ray)
 		self.traverser.addCollider(self.collision_camera, self.handler)
-		self.traverser.showCollisions(render)
-
+		#self.traverser.showCollisions(render)
 
 game = PIRATES()
 game.run()
