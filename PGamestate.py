1302645f3c81fdff8d3cb946c8398049d48f0eb1
Tarif Kayali <tkkayali@gmail.com>
2011-03-29 14:14:11 -0400

    Attempting to implement turns in combat

--- PGamestate.py
+++ PGamestate.py
@@ -26,26 +26,46 @@ class PIRATES(ShowBase):
 		base.disableMouse()	
 
 		self.limbo()
-		self.combat_collision_detection()
-		self.mouseTask = taskMgr.add(self.combat_mouse_task, 'combat_mouse_task')
-		
+
 		self.accept("escape", sys.exit)
 		self.accept("h", self.limbo_hide_all )
-		self.accept("c", self.combat )
-		self.accept("l", self.combat_hide_all )
+		self.accept("c", self.setup_combat )
+		self.accept("p", self.play_combat_system )
+		self.accept("a", self.combat_hide_all )
 		self.accept("z", self.limbo )
 
+		self.accept("g", self.printCamera )
+		
+	def printCamera(self):	
+		print self.camera.getX()
+		print self.camera.getY()
+		print self.camera.getZ()
+
+
 	def limbo(self):
+		#Set up the camera for the Limbo Menu
+		self.taskMgr.add(self.limbo_camera_task, "Limbo Camera")
+
 		#Set up the Limbo Sonatu
 		self.limbo_sonatu = self.loader.loadModel("Models\Sonatu\Sonatu.egg")
 		self.limbo_sonatu.setPos(0, 0, 0)
 		self.limbo_sonatu.setHpr(-90, 0, 0)
 		self.limbo_sonatu.reparentTo(self.render)
-		
-		#Set up the camera for the Limbo Menu
-		self.taskMgr.add(self.limbo_camera_task, "Limbo Camera")
 
-	def combat(self):
+		#Set up the characters
+		#Farthing
+		self.farthing = self.loader.loadModel("Models\Characters\Farthing\Farthing.egg")
+		self.farthing.setPos(-310.762, -3.750, -24.114)
+		self.farthing.setHpr(0, 0, 0)
+		self.farthing.reparentTo(self.render)
+		
+	def setup_combat(self):
+		#Set up camera for combat screen
+		self.taskMgr.add(self.combat_camera_task, "Combat Camera")
+
+		#Set up collision detection for combat
+		self.combat_collision_detection()
+
 		#Combat water
 		self.water = self.loader.loadModel("square.egg")
 		self.water.setSx(1000)
@@ -58,13 +78,22 @@ class PIRATES(ShowBase):
 
 		#Combat Sonatu
 		self.combat_sonatu = self.loader.loadModel("Models\Sonatu\Sonatu.egg")
-		self.combat_sonatu.setSx(.025)
-		self.combat_sonatu.setSy(.025)
-		self.combat_sonatu.setSz(.025)
-		self.combat_sonatu.setPos(0, 0, 1)
+		self.combat_sonatu.setSx(.022)
+		self.combat_sonatu.setSy(.022)
+		self.combat_sonatu.setSz(.022)
+		self.combat_sonatu.setPos(242.487, -90, 1)
 		self.combat_sonatu.lookAt(-1000, 0, 0)
 		self.combat_sonatu.reparentTo(self.render)
-		sonatu = Sonatu(0, 0, 0, self.combat_sonatu, 2)
+		self.sonatu = Sonatu(242.487, -90, 1, 2)
+
+		#Monster - Melee
+		self.melee_monster = self.loader.loadModel("Models\Monsters\octopus.egg")
+		self.melee_monster.setSx(0.35)
+		self.melee_monster.setSy(0.35)
+		self.melee_monster.setSz(0.35)
+		self.melee_monster.lookAt(-1000, 0, 0)
+		self.melee_monster.setPos(17.321, -90, 1)
+		self.melee_monster.reparentTo(self.render)
 
 		#Create the hex grid
 		self.gridspace_list = []
@@ -91,22 +120,39 @@ class PIRATES(ShowBase):
 		#Add a hex grid texture
 		self.map_grid = self.loader.loadModel("square.egg")
 		self.map_grid.setTexture(ts, loader.loadTexture("textures\HexGrid.png"))	
-		self.map_grid.setPos(20*sin(pi/3)*7.5, -15*6, 1)
-		self.map_grid.setSx(16*20*sin(pi/3))
-		self.map_grid.setSy(13.5*15)
+		self.map_grid.setPos(20*sin(pi/3)*7.5, -15*6, 0)
+		self.map_grid.setSx(16*20*sin(pi/3)+5)
+		self.map_grid.setSy(13.5*15+4)
 		self.map_grid.setTransparency(TransparencyAttrib.MAlpha)
 		self.map_grid.reparentTo(self.render)				
-
-		#Set up camera for combat
-		self.taskMgr.add(self.combat_camera_task, "Combat Camera")
 	
+	def play_combat_system(self):
+		combat_in_play = True
+		player_turn = True
+		if player_turn:
+			self.mouseTask = taskMgr.add(self.combat_mouse_task, 'combat_mouse_task')
+			#Add in ability to finish turn with remaining AP
+			if self.sonatu.getAP() is 0: 
+				player_turn = False
+		else:
+			self.mouseTask = taskMgr.removeTask('combat_mouse_task')
+			player_turn = True
+			self.melee_monster.setHpr(melee_monster, -90, 0, 0)
+			combat_in_play = False
+
+		return True
+		
+
+
 	def limbo_hide_all(self):
 		self.limbo_sonatu.hide()
+		self.farthing.hide()
 	
 	def combat_hide_all(self):
 		self.map_grid.hide()
 		self.water.hide()
 		self.combat_sonatu.hide()
+		self.melee_monster.hide()
 
 	def combat_mouse_task(self, task):
 		if base.mouseWatcherNode.hasMouse():
@@ -114,6 +160,9 @@ class PIRATES(ShowBase):
 		return Task.cont
 
 	def move_sonatu(self):
+		self.sonatu.setAP(self.sonatu.getAP()-1)
+		print self.sonatu.getAP()
+
 		if base.mouseWatcherNode.hasMouse():
 			self.mouse_position = base.mouseWatcherNode.getMouse()
 			self.collision_ray.setFromLens(base.camNode, self.mouse_position.getX(), self.mouse_position.getY())
@@ -144,7 +193,7 @@ class PIRATES(ShowBase):
 		self.collision_ray = CollisionRay()
 		self.collision_node.addSolid(self.collision_ray)
 		self.traverser.addCollider(self.collision_camera, self.handler)
-		self.traverser.showCollisions(render)
+		#self.traverser.showCollisions(render)
 
 game = PIRATES()
 game.run()
